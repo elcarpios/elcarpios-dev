0a5ccb6e4bed725a37b9a6cf0de9fbc2937fd255
elcarpios <elcarpiosbit@gmail.com>
2024-02-13 10:58:32 +0100

    Update astro to 4, redesign with section sticky headers

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -43,7 +43,7 @@
           type="text"
           class:border-red-700={formErrors.includes('name')}
           class:dark:border-red-700={formErrors.includes('name')}
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+          class=" bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           placeholder="John Doe"
         />
       </div>
@@ -57,7 +57,7 @@
           type="email"
           class:border-red-700={formErrors.includes('email')}
           class:dark:border-red-700={formErrors.includes('email')}
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+          class=" bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           placeholder="username@email.com"
           
         />
@@ -73,7 +73,7 @@
         rows={6}
         class:border-red-700={formErrors.includes('message')}
         class:dark:border-red-700={formErrors.includes('message')}
-        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg  border focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
         placeholder="Hello Victor! "></textarea>
     </div>
     <button
